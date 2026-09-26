c333c28a21a5361db28fddfe0d22f8f0bd31b001
Nelson E Diaz P <neslondiaze@gmail.com>
2026-09-26 10:11:50 -0400

    Add ic_cat_patient vector icon and update Gestion de Pacientes in MainActivity and Help Center (activity_category.xml)

--- android/app/src/main/java/ve/guiafarmaceutica/app/ui/MainActivity.java
+++ android/app/src/main/java/ve/guiafarmaceutica/app/ui/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         categories.add(new Category("Índice Alfabético", R.drawable.ic_cat_alphabet, CategoryViewModel.TYPE_ALFABETICO));
         categories.add(new Category("Impresión Diagnóstica", R.drawable.ic_cat_monography, "cat_impresion_diagnostica"));
         categories.add(new Category("Calculadora de Dosis", R.drawable.ic_pill, "cat_calculadora"));
-        categories.add(new Category("Gestión de Pacientes", R.drawable.ic_cat_security, "cat_gestion_pacientes"));
+        categories.add(new Category("Gestión de Pacientes", R.drawable.ic_cat_patient, "cat_gestion_pacientes"));
         categories.add(new Category("Configuración", R.drawable.ic_cat_settings, "cat_settings"));
         categories.add(new Category("Centro de Ayuda", R.drawable.ic_cat_help, CategoryViewModel.TYPE_AYUDA));
 
